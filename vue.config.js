403d81899bcb50f8a534a318f204715263624b08
wanghuayu <583440138@qq.com>
2019-05-31 17:41:11 +0800

    修改vue.config.js的缩进

--- vue.config.js
+++ vue.config.js
@@ -7,36 +7,36 @@ app.use('/api', express.Router())
 const login = require('./mock/login.json')
 
 function resolve(dir) {
-	return path.join(__dirname, dir)
+  return path.join(__dirname, dir)
 }
 module.exports = {
-	outputDir: 'dist',
-	configureWebpack: {
-		resolve: {
-			alias: {
-				'@': resolve('src'),
-				'api': resolve('src/api'),
-				'assets': resolve('src/assets'),
-				'base': resolve('src/base'),
-				'components': resolve('src/components'),
-				'utils': resolve('src/utils')
-			}
-		}
-	},
-	devServer: {
-		port: 4400,
-		proxy: {
-			'/api': {
-				target: 'http://localhost:4400/mock',
-				ws: true,
-				changeOrigin: true
-			}
-		},
-		before(app) {
-			app.post('/api/login', (req, res) => {
-				res.json(login)
-			})
-		}
-	},
-	lintOnSave: true
+  outputDir: 'dist',
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': resolve('src'),
+        'api': resolve('src/api'),
+        'assets': resolve('src/assets'),
+        'base': resolve('src/base'),
+        'components': resolve('src/components'),
+        'utils': resolve('src/utils')
+      }
+    }
+  },
+  devServer: {
+    port: 4400,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4400/mock',
+        ws: true,
+        changeOrigin: true
+      }
+    },
+    before(app) {
+      app.post('/api/login', (req, res) => {
+        res.json(login)
+      })
+    }
+  },
+  lintOnSave: true
 }
